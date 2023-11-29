75f554cc872ae0194d2adecc0ba650f83f9b43b7
Arindam Kabir <arindamkabir@gmail.com>
2023-11-30 03:53:15 +0600

    feat: update size list query hook

---
+++ hooks/queries/size/useGetSizeList.ts
@@ -0,0 +1,26 @@
+import axios from "@/lib/axios";
+import { PaginatedResponse } from "@/types/Response";
+import { Size } from "@/types/Size";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+
+export type SizeListRequest = {
+    search: string,
+    page: number,
+}
+
+export type SizeListResponse = PaginatedResponse<Size>;
+
+const fetchSizeList = async (params: SizeListRequest): Promise<SizeListResponse> => {
+    const response = await axios.get<SizeListResponse>(`/api/admin/sizes`, { params: params });
+    console.log(response);
+    return response.data;
+};
+
+export const useGetSizeList = (params: SizeListRequest) => {
+    return useQuery<SizeListResponse, Error>({
+        queryKey: ['sizes', params],
+        queryFn: () => {
+            return fetchSizeList(params);
+        }
+    });
+};
