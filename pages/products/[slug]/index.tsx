12193ba72e467645c69dc33eadba83d632dfb7d6
Arindam Kabir <arindamkabir@gmail.com>
2023-12-15 08:43:24 +0600

    fix: last try

--- pages/products/[slug]/index.tsx
+++ pages/products/[slug]/index.tsx
@@ -21,7 +21,7 @@ export const getServerSideProps = (async (context) => {
     try {
         const response = await axios.get<{ product: Product & { extra_images: ProductImage<null>[] } }>(`/api/admin/products/${slug}`, {
             headers: {
-                origin: req.headers.host,
+                origin: process.env.NEXT_PUBLIC_APP_ORIGIN ?? 'localhost:3000',
                 Cookie: req.headers.cookie
                 // ...req.headers
             }
